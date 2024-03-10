478fb09795df398a11f36a8771ee9aff781ede03
Akopyan Timur <56245133+sellerofair@users.noreply.github.com>
2024-03-10 09:19:46 +0300

    refactoring

--- lesson_1/problem_A/trees.py
+++ lesson_1/problem_A/trees.py
@@ -1,24 +1,28 @@
+# Вычисляет кличество деревьем в однм интервале
 def count_trees_in_section(start, end):
     return abs(end - start) + 1
 
 def count_trees(P, V, Q, M):
+
+    # Может покрасить первый
     P_start = P - V
     P_end = P + V
     P_count = count_trees_in_section(P_end, P_start)
 
+    # Может покрасить второй
     Q_start = Q - M
     Q_end = Q + M
     Q_count = count_trees_in_section(Q_end, Q_start)
 
-    # Intersection compensation calculating
+    # Количество общих дееревьев
     max_start = max(P_start, Q_start)
     min_end = min(P_end, Q_end)
-
     if max_start <= min_end:
         compensation = count_trees_in_section(min_end, max_start)
     else:
         compensation = 0
 
+    # Из общего количества вычитаются общие деревья
     result = P_count + Q_count - compensation
 
     return result
--- lesson_1/problem_B/commentator.py
+++ lesson_1/problem_B/commentator.py
@@ -1,25 +1,33 @@
 def count_goals_for_victory(first_game_score, current_score, first_game_owner):
+
+    # Получение счета в числах
     (first_game_friend_score, first_game_enemy_score) = list(map(int, first_game_score.split(':')))
     (current_game_friend_score, current_game_enemy_score) = list(map(int, current_score.split(':')))
     first_game_owner = int(first_game_owner)
 
+    # Вычисление форы
     first_game_handicap = first_game_friend_score - first_game_enemy_score
     current_game_handicap = current_game_friend_score - current_game_enemy_score
 
+    # Если общая фора больше нуля, уже победа, голы не нужны
     total_handicap = first_game_handicap + current_game_handicap
     if total_handicap > 0:
         return 0
 
+    # Количество голов для ничьей
     goals_to_draw = total_handicap * -1
-    result = goals_to_draw + 1
 
+    # Вычисление преимущества по голам в гостях
     if first_game_owner == 1:
         away_goals_advantage = (current_game_friend_score + goals_to_draw) - first_game_enemy_score
     else:
         away_goals_advantage = first_game_friend_score - current_game_enemy_score
 
-    if away_goals_advantage > 0:
-        result -= 1
+    # Если есть преимущество по голам в гостях, достаточно ничьей
+    # Если нет, нужна только победа и добавляется один гол
+    result = goals_to_draw
+    if away_goals_advantage <= 0:
+        result += 1
 
     return result
 
