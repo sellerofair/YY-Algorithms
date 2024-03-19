adee0790092a2657337f8a192cd2fa3606a53db5
Akopyan Timur <56245133+sellerofair@users.noreply.github.com>
2024-03-19 20:36:34 +0300

    refactoring

--- lesson_1/problem_G/barracks_destruction.py
+++ lesson_1/problem_G/barracks_destruction.py
@@ -4,16 +4,34 @@
 
 # По максимуму урон на бараки
 # Остальное на воинов в бараках
-def attack_barracks_health(attackers_power, barracks_health, barracks_productivity, barracks_power):
-    remained_power = max(attackers_power - barracks_health, 0)
-    barracks_health -= attackers_power - remained_power
-    barracks_power -= remained_power
+def attack_barracks_health(game_state):
+    attackers_power = game_state['attackers_power']
 
-    return barracks_health, barracks_power
+    remained_power = max(attackers_power - game_state['barracks_health'], 0)
+    game_state['barracks_health'] -= attackers_power - remained_power
+    game_state['barracks_power'] -= remained_power
 
-# Уничтожаются все воины в бараках
-# Остальные воины атакуют бараки
-def attack_barracks_power(attackers_power, barracks_health, barracks_productivity, barracks_power):
+# По максимуму урон на воинов в бараках
+# Остальное на бараки
+def attack_barracks_power(game_state):
+    attackers_power = game_state['attackers_power']
+
+    remained_power = max(attackers_power - game_state['barracks_power'], 0)
+    game_state['barracks_power'] -= attackers_power - remained_power
+    game_state['barracks_health'] -= remained_power
+
+def attack_barracks(game_state):
+    attackers_power = game_state['attackers_power']
+    barracks_health = game_state['barracks_health']
+    barracks_productivity = game_state['barracks_productivity']
+    barracks_power = game_state['barracks_power']
+    print(game_state)
+    # Если сила атакующих не больше, чем производят бараки,
+    # Шанс только по-максимуму бить казармы
+    if attackers_power <= barracks_productivity:
+        print('weak power')
+        attack_barracks_health(game_state)
+        return
 
     # Расчет возможности уничтожить казармы
     barracks_destruction_available = False
@@ -22,44 +40,35 @@ def attack_barracks_power(attackers_power, barracks_health, barracks_productivit
         remained_barracks_power = barracks_power - remained_power
         barracks_destruction_available = attackers_power >= remained_barracks_power * 2
 
+    print('barracks_destruction_available:', barracks_destruction_available)
     if barracks_destruction_available:
-        barracks_health_damage = barracks_health
-        barracks_power_damage = attackers_power - barracks_health_damage
+        attack_barracks_health(game_state)
     else:
-        barracks_power_damage = barracks_power
-        barracks_health_damage = attackers_power - barracks_power_damage
-
-    barracks_health -= barracks_health_damage
-    barracks_power -= barracks_power_damage
-
-    return barracks_health, barracks_power
-
+        attack_barracks_power(game_state)
 
 def destruct_barracks(attackers_power, barracks_health, barracks_productivity):
-    barracks_power = 0
-
-    # Выбор стратегии
-    if attackers_power > barracks_productivity:
-        attac_barracks = attack_barracks_power
-    else:
-        attac_barracks = attack_barracks_health
+    game_state = {
+        'attackers_power': attackers_power,
+        'barracks_health': barracks_health,
+        'barracks_productivity': barracks_productivity,
+        'barracks_power': 0
+    }
 
     rounds_count = 0
-    while barracks_health > 0 or barracks_power > 0:
+    while game_state['barracks_health'] > 0 or game_state['barracks_power'] > 0:
         rounds_count += 1
 
         # Ход нападающих
-        barracks_health, barracks_power = attac_barracks(attackers_power, barracks_health,
-                                                         barracks_productivity, barracks_power)
+        attack_barracks(game_state)
 
         # Ход казармы
-        attackers_power -= barracks_power
-        if attackers_power <= 0:
+        game_state['attackers_power'] -= game_state['barracks_power']
+        if game_state['attackers_power'] <= 0:
             return -1
 
         # Производство воинов казармой
-        if barracks_health > 0:
-            barracks_power += barracks_productivity
+        if game_state['barracks_health'] > 0:
+            game_state['barracks_power'] += game_state['barracks_productivity']
 
     return rounds_count
 
